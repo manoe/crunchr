5dd8ed8bf444a057168920140b8b7853cb2b962e
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2023-04-17 11:54:14 +0200

    Plot network with edges

--- plot_network/main.py
+++ plot_network/main.py
@@ -44,12 +44,15 @@ if __name__ == '__main__':
     g_nw = nx.DiGraph()
 
     for i in loader['loc_pdr']:
-        g_nw.add_node(i['node'], role=i['role'], master=i['master'], pos={'x': i['x'], 'y': i['y']})
+        g_nw.add_node(i['node'], role=i['role'], master=i['master'], pos=[i['x'], i['y']], state=i['state'])
         if 'routing_table' in i:
             for j in i['routing_table']:
                 if i['role'] == 'external':
                     pathid = [k['pathid'] for k in j['pathid']]
-                    g_nw.add_edge(i['node'], j['node'], pathid=pathid)
+                    g_nw.add_edge(i['node'], j['node'], pathid=pathid, secl=j['secl'])
                 else:
-                    g_nw.add_edge(i['node'], j['node'])
+                    g_nw.add_edge(i['node'], j['node'], secl=j['secl'])
 
+    nx.draw(g_nw, nx.get_node_attributes(g_nw, 'pos'))
+    plt.draw()
+    plt.show()
