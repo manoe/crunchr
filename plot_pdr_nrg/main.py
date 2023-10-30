e853f5c7149b22428b3797edf5ca0f75b12ee0c5
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2023-10-30 13:09:03 +0100

    +x

--- plot_pdr_nrg/main.py
+++ plot_pdr_nrg/main.py
@@ -6,8 +6,13 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
+
+def sum_list_prop(list_prop):
+    return [sum([x[list_prop] for x in i['nodes']]) for i in loader['nrg_list']]
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='plot_depletion', description='Plot node depletion over time', epilog=':-(')
+    parser = argparse.ArgumentParser(prog='plot_pdr_nrg', description='Plot network pdr and energy over time', epilog=':-(')
     parser.add_argument('filename', help='use ``-\'\' for stdin')
 
     args = parser.parse_args()
@@ -19,16 +24,22 @@ if __name__ == '__main__':
 
     loader = yaml.safe_load(stream)
 
-    arr = np.ndarray(shape=(len(loader['nrg_list']), len(loader['nrg_list'][0]['nodes'])))
-    arr.fill(0.0)
+    sent_t_arr = sum_list_prop('report_sent')
+    recv_t_arr = sum_list_prop('report_recv')
+    nrg_t_arr = sum_list_prop('energy')
 
-    x = []
-    idx = 0
-    for i in loader['nrg_list']:
-        x.append(i['timestamp'])
-        arr[idx][:] = [e['energy'] for e in i['nodes']]
-        idx += 1
+    sent_d_arr = np.subtract(sent_t_arr[1:], sent_t_arr[:-1])
+    recv_d_arr = np.subtract(recv_t_arr[1:], recv_t_arr[:-1])
+    nrg_d_arr = np.subtract(nrg_t_arr[:-1], nrg_t_arr[1:])
 
-    plt.plot(x, arr)
-    plt.ylim([0, 200])
+    timestamp = [round(i['timestamp'], 0) for i in loader['nrg_list']][:-1]
+
+    pdr_arr = np.divide(recv_d_arr, sent_d_arr)
+
+    nrg_eff_arr = np.multiply(pdr_arr, nrg_d_arr)
+
+    plt.plot(timestamp, np.transpose(np.array([nrg_eff_arr, nrg_d_arr])))
+    plt.legend(['Efficient energy consumed', 'Total energy consumed'])
+
     plt.show()
+
