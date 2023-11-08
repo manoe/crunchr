f28b2e46770fa1128057dc39fd912efe4f33c69a
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2023-11-08 10:03:41 +0100

    Update

--- plot_nrg_per_pkt_vs_size/main.py
+++ plot_nrg_per_pkt_vs_size/main.py
@@ -31,17 +31,9 @@ if __name__ == '__main__':
     y_time = {}
     y_time_std = {}
 
-    size_arr = args.size+args.size
-
-    size_arr = [int(x) for x in size_arr]
-
-    idx = 0
-
     for filename in files:
         y_nrg[filename] = {}
-        y_time[filename] = {}
         y_nrg_std[filename] = {}
-        y_time_std[filename] = {}
 
         for s in args.size:
             stream = open(filename+'_'+str(s)+'.yaml', 'r')
@@ -53,45 +45,11 @@ if __name__ == '__main__':
 
             pkt_list = {}
             fail = 0
-            data = []
 
-            for k in [args.percent]:
-                for j in loader['runs']:
-                    pkt_list = {}
-                    for i in j['pkt_list']:
-                        if i['source'] in pkt_list:
-                            pkt_list[i['source']] += 1
-                        else:
-                            pkt_list[i['source']] = 1
-                        if check_status(pkt_list, node, k, args.count):
-                            data.append({'timestamp': i['timestamp'],
-                                         'energy':    i['energy'],
-                                         'percent':  k})
-                            if args.data:
-                                print(str(i['timestamp']) + '#' + str(l) + '#' + str(i['energy']))
-                            else:
-                                print(str(k) + ' of active nodes reached at ' + str(i['timestamp'])+' with criteria of '
-                                      + str(args.count) + ' packets and ' + str(i['energy']) + ' J of energy')
-                            fail -= 1
-                            break
-                    fail += 1
+            data = [j['pkt_list'][-1]['energy']/len(j['pkt_list']) for j in loader['runs']]
 
-            if fail > 0:
-                print('Init criteria not met x'+str(fail)+' times')
-
-            y_nrg[filename][s] = np.average([x['energy'] for x in data if x['percent'] == args.percent])
-            y_nrg_std[filename][s] = np.std([x['energy'] for x in data if x['percent'] == args.percent])
-            y_time[filename][s] = np.average([x['timestamp'] for x in data if x['percent'] == args.percent])
-            y_time_std[filename][s] = np.std([x['timestamp'] for x in data if x['percent'] == args.percent])
-            idx += 1
-
-#    y_efmrp_nrg=list(y_nrg.values())[:len(args.size)]
-#    y_shmrp_nrg=list(y_nrg.values())[len(args.size):]
-
-#    y_efmrp_time = list(y_time.values())[:len(args.size)]
-#    y_shmrp_time = list(y_time.values())[len(args.size):]
-
-    plt.subplot(2,1,1)
+            y_nrg[filename][s] = np.average(data)
+            y_nrg_std[filename][s] = np.std(data)
 
     width = 0.8
     bar_width = width / len(y_nrg)
@@ -102,24 +60,14 @@ if __name__ == '__main__':
             plt.errorbar([x + idx*bar_width-width/2 for x in range(len(args.size))], list(y_nrg[data].values()),
                          list(y_nrg_std[data].values()), linestyle='None')
     else:
-        for i in y_nrg:
+        for idx, i in enumerate(y_nrg):
             bc = plt.bar([x + idx*bar_width-width/2 for x in range(len(args.size))] , y_nrg[i].values(), bar_width)
             plt.bar_label(bc, label=args.size)
             idx += 1
 
-    plt.title('Consumed energy vs. size')
+    plt.title('Consumed energy per packet vs. size')
     plt.xticks(range(len(args.size)), labels=args.size)
 
-    plt.subplot(2,1,2)
-
-    idx = 0
-    for i in y_nrg:
-        bc = plt.bar([x + idx*bar_width-width/2 for x in range(len(args.size))] , y_time[i].values(), bar_width)
-        plt.bar_label(bc, label=args.size)
-        idx += 1
-
-    plt.title('Elapsed time vs. size')
-    plt.xticks(range(len(args.size)), labels=args.size)
     plt.legend(y_time.keys())
     plt.show()
     exit(0)
