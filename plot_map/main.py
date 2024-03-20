e0f816c93e040fc0b1aef4b3cc0c338d47eafa20
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2024-03-20 16:00:20 +0100

    Support shelving for big data stuff
     Changes to be committed:
    	modified:   main.py

--- plot_map/main.py
+++ plot_map/main.py
@@ -8,6 +8,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yaml
 import pandas as pd
+import shelve as sh
+
+def shelve_out(filename, keys):
+    my_shelf = sh.open(filename, 'n')  # 'n' for new
+    for key in keys:
+        try:
+            my_shelf[key] = globals()[key]
+        except TypeError:
+            #
+            # __builtins__, my_shelf, and imported modules can not be shelved.
+            #
+            print('ERROR shelving: {0}'.format(key))
+        except KeyError:
+            print('ERROR shelving: {0}'.format(key))
+        except:
+            print('ERROR')
+    my_shelf.close()
+
+
+def shelve_in(filename):
+    my_shelf = sh.open(filename)
+    for key in my_shelf:
+        globals()[key] = my_shelf[key]
+    my_shelf.close()
 
 
 def calc_pdr(run):
@@ -35,18 +59,25 @@ if __name__ == '__main__':
     parser.add_argument('-ty', '--title-y', dest='title_y', action='store', type=str, default='Y')
     parser.add_argument('-f', '--file', dest='file', help='use the pattern blabla_px_py_babla.yaml')
     parser.add_argument('-t', '--tail', dest='tail', action='store', type=float, help='The tail threshold', default=0.0)
+    parser.add_argument('-s', '--shelve', dest='shelve', action='store_true', help='use the pattern blabla_px_py_babla.yaml')
     args = parser.parse_args()
 
     arr = []
 
-    for idx_q, q in enumerate(args.param_x):
-        y_arr = []
-        for idx_p, p in enumerate(args.param_y):
-            filename = args.file.replace('px', q).replace('py', p)
-            print('Opening file: ' + filename)
-            loader = yaml.safe_load(open(filename, 'r'))
-            y_arr.append(tail([calc_pdr(run) for run in loader['runs']], args.tail))
-        arr.append(y_arr)
+    if args.shelve:
+        shelve_in(args.file)
+    else:
+        for idx_q, q in enumerate(args.param_x):
+            y_arr = []
+            for idx_p, p in enumerate(args.param_y):
+                filename = args.file.replace('px', q).replace('py', p)
+                print('Opening file: ' + filename)
+                loader = yaml.safe_load(open(filename, 'r'))
+                y_arr.append([calc_pdr(run) for run in loader['runs']])
+            arr.append(y_arr)
+        shelve_out(args.file, ['arr'])
 
-    df = pd.DataFrame(arr, columns=args.param_y, index=args.param_x)
+    arr_tailed = [[tail(i, args.tail) for i in j] for j in arr]
+
+    df = pd.DataFrame(arr_tailed, columns=args.param_y, index=args.param_x)
     print(df.to_string())
