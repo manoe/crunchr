742b0761d8e34f4fac441da52b8f9ac0411712c7
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2024-03-25 11:12:18 +0100

    New metric - distance corrected PDR

--- plot_map/main.py
+++ plot_map/main.py
@@ -33,6 +33,8 @@ def shelve_in(filename):
         globals()[key] = my_shelf[key]
     my_shelf.close()
 
+def collect_node_pdr(run):
+    return {node['node']: node['report_pdr'] for node in run['pdr'] if 'report_pdr' in node}
 
 def calc_pdr(run):
     pdrs = []
@@ -42,8 +44,24 @@ def calc_pdr(run):
     return np.average(pdrs)
 
 
+def gen_node_loc(run):
+    return {i['node']: [i['x'], i['y']] for i in run['loc_pdr']}
+
+
+def gen_node_pdr(run):
+    return {i['node']: i['report_pdr'] for i in run['pdr'] if 'report_pdr' in i}
+
+
+def calc_dc_pdr(run):
+    loc = gen_node_loc(run)
+    pdr = gen_node_pdr(run)
+    d_max = max([np.linalg.norm(i) for i in loc.values()])
+    return np.average([np.linalg.norm(loc[i])/d_max*pdr[i] for i in pdr.keys()])
+
+
 def calc_connr(run):
     return len([node['node'] for node in run['pdr'] if 'report_pdr' in node and node['report_pdr'] != 0])/len([node['node'] for node in run['pdr'] if 'report_pdr' in node])
+
 
 def tail(arr, th):
     return len([i for i in arr if i > th])/len(arr)
@@ -62,11 +80,13 @@ if __name__ == '__main__':
     parser.add_argument('-ty', '--title-y', dest='title_y', action='store', type=str, default='Y')
     parser.add_argument('-f', '--file', dest='file', help='use the pattern blabla_px_py_babla.yaml', required=True)
     parser.add_argument('-t', '--tail', dest='tail', action='store', type=float, help='The tail threshold', default=0.0)
+    parser.add_argument('-p', '--plot', dest='plot', action='store', choices=['pdr', 'connr', 'dc_pdr'], help='The tail threshold')
     parser.add_argument('-s', '--shelve', dest='shelve', action='store_true', help='use the pattern blabla_px_py_babla.yaml')
     args = parser.parse_args()
 
     arr_pdr = []
     arr_connr = []
+    arr_dc_pdr = []
 
     if args.shelve:
         shelve_in(args.file)
@@ -74,25 +94,66 @@ if __name__ == '__main__':
         for idx_q, q in enumerate(args.param_x):
             y_pdr_arr = []
             y_connr_arr = []
+            y_dc_pdr_arr = []
             for idx_p, p in enumerate(args.param_y):
                 filename = args.file.replace('px', q).replace('py', p)
                 print('Opening file: ' + filename)
                 loader = yaml.safe_load(open(filename, 'r'))
                 y_pdr_arr.append([calc_pdr(run) for run in loader['runs']])
                 y_connr_arr.append([calc_connr(run) for run in loader['runs']])
+                y_dc_pdr_arr.append([calc_dc_pdr(run) for run in loader['runs']])
             arr_pdr.append(y_pdr_arr)
             arr_connr.append(y_connr_arr)
-        shelve_out(args.file, ['arr_pdr', 'arr_connr'])
+            arr_dc_pdr.append(y_dc_pdr_arr)
+        shelve_out(args.file, ['arr_pdr', 'arr_connr', 'arr_dc_pdr'])
 
     arr_pdr_tailed = [[tail(i, args.tail) for i in j] for j in arr_pdr]
     arr_connr_tailed = [[tail(i, args.tail) for i in j] for j in arr_connr]
+    arr_dc_pdr_tailed = [[tail(i, args.tail) for i in j] for j in arr_dc_pdr]
+
+    print(arr_dc_pdr)
 
     df_pdr = pd.DataFrame(arr_pdr_tailed, columns=args.param_y, index=args.param_x)
     df_connr = pd.DataFrame(arr_connr_tailed, columns=args.param_y, index=args.param_x)
+    df_dc_pdr = pd.DataFrame(arr_dc_pdr_tailed, columns=args.param_y, index=args.param_x)
     print('PDRs with tail set to '+str(args.tail))
     print(df_pdr.to_string())
-    print('\n')
     print('Connection ratio with tail set to '+str(args.tail))
     print(df_connr.to_string())
+    print('Distance corrected PDR with tail set to ' + str(args.tail))
+    print(df_dc_pdr.to_string())
 
+    if args.plot is None:
+        exit(0)
 
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(4, 4))
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+
+    if 'pdr' == args.plot:
+        img = arr_pdr_tailed
+        ax.set_title('PDR with tail='+str(args.tail))
+    elif 'connr' == args.plot:
+        img = arr_connr_tailed
+        ax.set_title('Connection ratio with tail=' + str(args.tail))
+    elif 'dc_pdr' == args.plot:
+        img = arr_dc_pdr_tailed
+        ax.set_title('Distance corrected PDR with tail=' + str(args.tail))
+    else:
+        print('Error, unknown plot source')
+        exit(-1)
+
+    ax.imshow(img)
+    for i in range(len(args.param_x)):
+        for j in range(len(args.param_y)):
+            text = ax.text(j, i, "{:.3f}".format(img[i][j]), ha="center", va="center", fontsize='small')
+
+    ax.set_xticks(np.arange(len(args.param_y)), labels=args.param_y)
+    ax.set_yticks(np.arange(len(args.param_x)), labels=args.param_x)
+    ax.set_xlabel(r"{}".format(args.title_y))
+    ax.set_ylabel(r"{}".format(args.title_x))
+
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    fig.tight_layout()
+    plt.show()
