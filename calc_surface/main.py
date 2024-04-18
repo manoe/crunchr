36ae8f2107c702d2a7c200d3192d72bc5919276f
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2024-04-18 14:32:50 +0200

    Prepare for mass run

--- calc_surface/main.py
+++ calc_surface/main.py
@@ -5,6 +5,31 @@ import matplotlib.pyplot as plt
 import argparse
 import yaml
 import math
+import shelve as sh
+
+
+def shelve_out(filename, keys):
+    my_shelf = sh.open(filename, 'n')  # 'n' for new
+    for key in keys:
+        try:
+            my_shelf[key] = globals()[key]
+        except TypeError:
+            #
+            # __builtins__, my_shelf, and imported modules can not be shelved.
+            #
+            print('ERROR shelving: {0}'.format(key))
+        except KeyError:
+            print('ERROR shelving: {0}'.format(key))
+        except:
+            print('ERROR')
+    my_shelf.close()
+
+
+def shelve_in(filename):
+    my_shelf = sh.open(filename)
+    for key in my_shelf:
+        globals()[key] = my_shelf[key]
+    my_shelf.close()
 
 # Making a class circle and initializing it with its centre and radius
 class circle:
@@ -37,6 +62,14 @@ def gen_node_pdr(run):
     return {i['node']: i['report_pdr'] for i in run['pdr'] if 'report_pdr' in i}
 
 
+def calc_pdr(run):
+    pdrs = []
+    for node in run['pdr']:
+        if 'report_pdr' in node:
+            pdrs.append(node['report_pdr'])
+    return np.average(pdrs)
+
+
 def calc_dc_pdr(run):
     loc = gen_node_loc(run)
     pdr = gen_node_pdr(run)
@@ -65,83 +98,83 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--file', dest='file', help='use the pattern blabla_px_py_babla.yaml', required=True)
     parser.add_argument('-l', '--length', dest='length', help='Side length of the area', default=160, type=float)
     parser.add_argument('-r', '--radius', dest='radius', help='Radius ', default=5, type=float)
-#    parser.add_argument('-t', '--tail', dest='tail', action='store', type=float, help='The tail threshold', default=0.0)
-#    parser.add_argument('-p', '--plot', dest='plot', action='store', choices=['pdr', 'connr', 'dc_pdr', 'd_p'],
-#                        help='Kind of the plot')
-#    parser.add_argument('-s', '--shelve', dest='shelve', action='store_true',
-#                        help='use the pattern blabla_px_py_babla.yaml')
+    parser.add_argument('-t', '--threshold', dest='threshold', action='store', type=float, help='The error threshold', default=0.01)
+    parser.add_argument('-p', '--plot', dest='plot', action='store_true', help='Let\'s plot')
+    parser.add_argument('-s', '--shelve', dest='shelve', action='store_true',
+                        help='use the pattern blabla_px_py_babla.yaml')
     args = parser.parse_args()
 
-    print('Opening file: ' + args.file)
-    loader = yaml.safe_load(open(args.file, 'r'))
+    if args.shelve:
+        shelve_in(args.file+'.dat')
+    else:
+        print('Opening file: ' + args.file)
+        loader = yaml.safe_load(open(args.file, 'r'))
+        res = []
+        for run in loader['runs']:
+            loc = gen_node_loc(run)
+            conn = gen_node_conn(loader['runs'][0])
+            x = np.linspace(0, args.length, 300)
+            y = np.linspace(0, args.length, 300)
+            # 100:  0.4119
+            # 200:  0.41235
+            # 300:  0.4096222222222222
+            # 500:  0.408752
+            # 1000: 0.408165
+            xx, yy = np.meshgrid(x, y)
+            err = 1.0
+            th  = args.threshold
+            r_min = 0
+            r_max = math.sqrt(2)*args.length
+            r_arr = np.linspace(r_min, r_max, 200)
+            r_idx = len(r_arr) - 1
+            r_idx_left = 0
+            r_idx_right = len(r_arr) - 1
+            while True:
+                circles = [circle(r_arr[r_idx], loc[i][0], loc[i][1]) for i in loc.keys() if conn[i]]
+                c = [in_area((x,y)) for x, y in zip(np.ravel(xx), np.ravel(yy))]
+                area = sum(c) / len(c)
+                err = 1.0 - area
+                print('=====================')
+                if err > th:
+                    print('Error higher: '+str(err))
+                    r_idx_new = int(r_idx_right-((r_idx_right-r_idx)/2))
+                    r_idx_left = r_idx
+                elif err <= th:
+                    if area < 1.0:
+                        print('Done. Error: '+str(err))
+                        break
+                    else:
+                        print('Error lower: '+str(err))
+                        r_idx_new = int(r_idx_left+(r_idx-r_idx_left)/2)
+                        r_idx_right = r_idx
+                print('Index: ' + str(r_idx))
+                print('Area:  ' + str(area))
+                print('New index: ' + str(r_idx_new))
+                print('Idx left: ' +str(r_idx_left))
+                print('Idx right: ' + str(r_idx_right))
 
-    loc = gen_node_loc(loader['runs'][0])
+                if r_idx_new == r_idx:
+                    break
+                else:
+                    r_idx = r_idx_new
+            res.append({'seed': run['seed'], 'pdr': calc_pdr(run), 'dc-pdr': calc_dc_pdr(run), 'radius': r_arr[r_idx]})
 
-    conn = gen_node_conn(loader['runs'][0])
-    print(conn)
+        shelve_out(args.file+'.dat',['res', 'circles', 'area', 'c', 'xx', 'yy', 'args'])
 
-    fig, ax = plt.subplots()
-    ax.set(xlim=(0, args.length), ylim=(0, args.length))
+    print(res)
+    if args.plot:
+        fig, ax = plt.subplots()
+        ax.set(xlim=(0, args.length), ylim=(0, args.length))
+        colors = list(map(lambda x: {True: 'b', False: 'w'}[x], c))
+        plt.scatter(np.ravel(xx), np.ravel(yy), c=colors)
+        plt.title(f'Area: {area}')
+        for c in circles:
+            a_circle = plt.Circle((c.x, c.y), c.radius, color='r', linewidth=1, fill=False)
+            ax.add_artist(a_circle)
+        ax.add_patch(plt.Rectangle((0, 0), args.length, args.length, color="k", linewidth=1, fill=False))
 
-    x = np.linspace(0, args.length, 300)
-    y = np.linspace(0, args.length, 300)
-    # 100:  0.4119
-    # 200:  0.41235
-    # 300:  0.4096222222222222
-    # 500:  0.408752
-    # 1000: 0.408165
+        # Printing the graph
+        plt.show()
 
-    xx, yy = np.meshgrid(x, y)
-    print(x.shape, y.shape, xx.shape, yy.shape)
+        # plotting circles and a square from (0,0) to (1,1)
 
-    err = 1.0
-    th  = 0.01
-    r_min = 0
-    r_max = math.sqrt(2)*args.length
-    r_arr = np.linspace(r_min, r_max, 100)
-    r_idx = len(r_arr) - 1
-    r_idx_left = 0
-    r_idx_right = len(r_arr) - 1
-    while True:
-        circles = [circle(r_arr[r_idx], loc[i][0], loc[i][1]) for i in loc.keys() if conn[i]]
-        c = [in_area((x,y)) for x, y in zip(np.ravel(xx), np.ravel(yy))]
-        area = sum(c) / len(c)
-        err = 1.0 - area
-        print('=====================')
-        if err > th:
-            print('Error higher: '+str(err))
-            r_idx_new = int(r_idx_right-((r_idx_right-r_idx)/2))
-            r_idx_left = r_idx
-        elif err <= th:
-            if area < 1.0:
-                print('Done')
-                break
-            else:
-                print('Error lower: '+str(err))
-                r_idx_new = int(r_idx_left+(r_idx-r_idx_left)/2)
-                r_idx_right = r_idx
-        print('Index: ' + str(r_idx))
-        print('Area:  ' + str(area))
-        print('New index: ' + str(r_idx_new))
-        print('Idx left: ' +str(r_idx_left))
-        print('Idx right: ' + str(r_idx_right))
-
-        if r_idx_new == r_idx:
-            break
-        else:
-            r_idx = r_idx_new
-
-
-    colors = list(map(lambda x: {True: 'b', False: 'w'}[x], c))
-    plt.scatter(np.ravel(xx), np.ravel(yy), c=colors)
-    plt.title(f'Area: {area}')
-
-
-    # plotting circles and a square from (0,0) to (1,1)
-    for c in circles:
-        a_circle = plt.Circle((c.x, c.y), c.radius, color='r', linewidth=1, fill=False)
-        ax.add_artist(a_circle)
-    ax.add_patch(plt.Rectangle((0, 0), args.length, args.length, color="k", linewidth=1, fill=False))
-
-    # Printing the graph
-    plt.show()
