293cfa5b43dece05148cc1e8686f5465754ad4d8
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2024-11-12 09:29:17 +0100

    write to pickle

--- process_border/main.py
+++ process_border/main.py
@@ -1,12 +1,7 @@
 #!/bin/env python3
 
 import yaml
-import matplotlib.pyplot as plt
 import argparse
-import sys
-import math
-import numpy as np
-from numpy.ma.core import ravel, arange
 import pandas as pd
 
 
@@ -45,56 +40,24 @@ def get_hop_pkt_stat(x_bars):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='border_plot', description='Plot MSR2MRP border node related stats', epilog=':-(')
-    parser.add_argument('filename', help='use ``-\'\' for stdin', nargs='*')
+    parser.add_argument('filename', help='Input filenames', nargs='*')
     parser.add_argument('-c', '--columns', dest='columns', type=int, default=2, help='Number of columns in plot')
+    parser.add_argument('-o', '--out', dest='out', type=str, default='out', help='Output filename')
     args = parser.parse_args()
 
-    if len(args.filename) > 1:
-        means = []
-        for filename in args.filename:
-            stream = open(filename, 'r')
-            loader = yaml.safe_load(stream)
-            borders = get_borders(loader)
-            hop_pkt_list = get_hop_pkt_list(borders)
-            table = get_hop_pkt_stat(hop_pkt_list)
-            means.append({'hop': table.index, 'mean': table.mean(axis=1)})
-        print(means)
-        max_hop = max([max(i['hop']) for i in means])
-        table = pd.DataFrame(index=list(range(1, max_hop + 1)), columns=range(len(means)))
-        for m_idx, m in enumerate(means):
-            for idx,i in enumerate(m['hop']):
-                table.at[i, m_idx] = m['mean'][i]
-        table.fillna(0)
-        fig, ax = plt.subplots(1, 1)
-        print(table) # .std(axis=1)
-        ax.bar(table.index, table.mean(axis=1), yerr=table.std(axis=1), align='center', ecolor='black', capsize=10)
-#        ax.errorbar(table.index, table.mean(axis=1), table.std(axis=1), fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4,
-#                     capsize=2)
-        ax.title.set_text('Average hop/pkt')
-        plt.tight_layout()
-        plt.show()
-    else:
-        stream = sys.stdin
-        if args.filename[0] != '-':
-            stream = open(args.filename[0], 'r')
+    means = []
+    for filename in args.filename:
+        stream = open(filename, 'r')
         loader = yaml.safe_load(stream)
         borders = get_borders(loader)
-
         hop_pkt_list = get_hop_pkt_list(borders)
-        nrows = math.ceil((len(hop_pkt_list) + 1) / 2)
-        ncols = args.columns
-        fig, axs = plt.subplots(nrows=nrows, ncols=ncols)
-        ax_arr = axs.ravel()
-
-        for idx,i in enumerate(hop_pkt_list):
-            ax_arr[idx].bar(i['list'].keys(), i['list'].values())
-            ax_arr[idx].title.set_text('Node '+str(i['node']))
-            ax_arr[idx].set_xticks(np.arange(len(i['list'].keys())+1))
-
         table = get_hop_pkt_stat(hop_pkt_list)
+        means.append({'hop': table.index, 'mean': table.mean(axis=1)})
+    max_hop = max([max(i['hop']) for i in means])
+    table = pd.DataFrame(index=list(range(1, max_hop + 1)), columns=range(len(means)))
+    for m_idx, m in enumerate(means):
+        for idx,i in enumerate(m['hop']):
+            table.at[i, m_idx] = m['mean'][i]
+    table.fillna(0)
 
-        ax_arr[len(borders)].bar(table.index, table.mean(axis=1))
-        ax_arr[len(borders)].title.set_text('Average hop/pkt')
-
-        plt.tight_layout()
-        plt.show()
+    table.to_pickle(args.out+'.pickle')
