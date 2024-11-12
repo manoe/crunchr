f416bc66fa8ae189862fbe67edf5340debd61ee5
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2024-11-12 11:10:41 +0100

    some details

--- plot_border/main.py
+++ plot_border/main.py
@@ -46,10 +46,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='plot_border', description='Plot MSR2MRP border node related pickleized stats', epilog=':-(')
     parser.add_argument('filename', help='Pickle(s) to read', nargs='+')
     parser.add_argument('-c', '--columns', dest='columns', type=int, default=2, help='Number of columns in plot')
-    parser.add_argument('-t', '--title', dest='title', type=str, default='MSR2MRP', help='Plot title(s)', nargs='+')
+    parser.add_argument('-t', '--title', dest='title', type=str, default=['MSR2MRP'], help='Plot title(s)', nargs='+')
     args = parser.parse_args()
 
     if len(args.filename) != len(args.title) and len(args.title) != 1:
+        print(len(args.title))
         print('Filename and title must be the same amount')
         exit(1)
 
@@ -58,10 +59,11 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(nrows=nrows, ncols=ncols)
     ax_list = ax.ravel()
 
-    for f_idx,filename in args.filename:
+    for f_idx,filename in enumerate(args.filename):
         table = pd.read_pickle(filename)
         ax_list[f_idx].bar(table.index, table.mean(axis=1), yerr=table.std(axis=1), align='center', ecolor='black', capsize=10)
         ax_list[f_idx].title.set_text(args.title[f_idx]+' - average hop/pkt')
 
+    
     plt.tight_layout()
     plt.show()
