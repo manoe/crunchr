0d2c2e18d6d994785361a030bd4bf27dafc0a7de
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2024-12-20 10:23:55 +0100

    Some new stuff

--- msr2mrp_meas_1_plot/main.py
+++ msr2mrp_meas_1_plot/main.py
@@ -6,9 +6,14 @@ logger = logging.getLogger(__name__)
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import numpy as np
 import networkx as nx
 import yaml
+
+from matplotlib import rcParams
+rcParams['font.family'] = ['serif']
+rcParams['font.serif'] = ['Times New Roman']
 
 def construct_graph(run):
     nw = nx.MultiDiGraph()
@@ -44,11 +49,16 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--parameters', dest='params', nargs='+', help='Parameter space')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Debug mode')
     parser.add_argument('-pl', '--plot', dest='plot', choices=['network', 'bar'], help='Plot type')
-    parser.add_argument('-n', '--network', dest='network', type=str, help='Number of nodes')
+    parser.add_argument('-pd', '--plot-data', dest='plot_data', choices=['disjoint', 'pathnum'], default='disjoint', help='Plot data')
+    parser.add_argument('-n', '--network', dest='network', type=str, nargs='*', help='Number of nodes')
     args = parser.parse_args()
 
     if args.debug:
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    if args.plot == 'network' and len(args.params) != len(args.network):
+        logger.error('Number of parameters and number of networks must match')
+        exit(1)
 
     record = []
 
@@ -60,45 +70,66 @@ if __name__ == '__main__':
         dmp = pd.read_pickle(filename.replace('$2', 'dmp'))
         rm = pd.read_pickle(filename.replace('$2', 'rm'))
         sink = pd.read_pickle(filename.replace('$2', 'sink'))
-        record.append({'filename': filename, 'dmp':dmp, 'rm':rm, 'sink':sink})
+        iso  = pd.read_pickle(filename.replace('$2', 'sink'))
+        record.append({'filename': filename, 'dmp':dmp, 'rm':rm, 'sink':sink, 'iso':iso})
 
     if args.plot == 'bar':
-        fig, axs = plt.subplots(nrows=3, ncols=1)
+        fig, axs = plt.subplots(nrows=4, ncols=1)
         sink_num = [ np.average(r['sink'].mean(axis=1)) for r in record ]
         sink_err = [ np.average(r['sink'].std(axis=1)) for r in record ]
         axs[0].set_title('Sink')
-        axs[0].bar(args.params, sink_num)
+        bars = axs[0].bar(args.params, sink_num)
+        axs[0].bar_label(bars, label_type='edge')
         axs[0].errorbar(args.params, sink_num, sink_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4, capsize=2)
 
         path_num = [ np.average(r['rm'].mean(axis=1)) for r in record ]
         path_err = [ np.average(r['rm'].std(axis=1)) for r in record ]
         axs[1].set_title('Path')
-        axs[1].bar(args.params, path_num)
+        bars = axs[1].bar(args.params, path_num)
+        axs[1].bar_label(bars, label_type='edge')
         axs[1].errorbar(args.params, path_num, path_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4, capsize=2)
 
         dmp_num = [np.average(r['dmp'].mean(axis=1)) for r in record]
         dmp_err = [np.average(r['dmp'].std(axis=1)) for r in record]
 
         axs[2].set_title('Disjointness')
-        axs[2].bar(args.params, dmp_num)
+        bars = axs[2].bar(args.params, dmp_num)
+        axs[2].bar_label(bars, label_type='edge')
         axs[2].errorbar(args.params, dmp_num, dmp_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4,
                      capsize=2)
+
+        two_disj_num = [ (r['dmp'] ==0).sum(axis=1).div(r['dmp'].notna().sum(axis=1)).mean() for r in record ]
+        two_disj_err =[ (r['dmp'] ==0).sum(axis=1).div(r['dmp'].notna().sum(axis=1)).std() for r in record ]
+
+        axs[3].set_title('Two disj probability')
+        bars = axs[3].bar(args.params, two_disj_num)
+        axs[3].errorbar(args.params, two_disj_num, two_disj_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4,
+                     capsize=2)
+        axs[3].bar_label(bars, label_type='edge')
         plt.tight_layout()
         plt.show()
     elif args.plot == 'network':
-        fig, axs = plt.subplots(nrows=1, ncols=1)
+        fig, axs = plt.subplots(nrows=int(np.ceil(len(record[1:]) / 2)), ncols=2)
+        axs_arr = axs.ravel()
 
-        stream = open(args.network, 'r')
-        loader = yaml.safe_load(stream)
-        nw = construct_graph(loader)
+        for idx_r,r in enumerate(record[1:]):
+            stream = open(args.network[idx_r+1], 'r')
+            loader = yaml.safe_load(stream)
+            nw = construct_graph(loader)
+            if args.plot_data == 'disjoint':
+                color = [r['dmp'].mean(axis=0)[i] if i in r['dmp'].mean(axis=0).index else 0 for i in nw.nodes()]
+            else:
+                color = [r['rm'].mean(axis=0)[i] if i in r['rm'].mean(axis=0).index else 0 for i in nw.nodes()]
 
-        record[3]['dmp'].mean(axis=0)
+            pos = nx.get_node_attributes(nw, 'pos')
 
-        color=[ record[3]['dmp'].mean(axis=0)[i] if i in record[3]['dmp'].mean(axis=0).index else 0 for i in nw.nodes() ]
+            nx.draw_networkx_nodes(nw, ax=axs_arr[idx_r], pos=pos, node_color=color, cmap='viridis', node_size=200)
+            nx.draw_networkx_edges(nw, ax=axs_arr[idx_r], pos=pos)
 
-        nx.draw_networkx_nodes(nw, ax=axs, pos=nx.get_node_attributes(nw, 'pos'), node_color=color, cmap='viridis')
-        nx.draw_networkx_edges(nw, ax=axs, pos=nx.get_node_attributes(nw, 'pos'))
-
+            cmap = cm.ScalarMappable(cmap='viridis')
+            cmap.set_clim(vmin=np.min(color), vmax=np.max(color))
+            fig.colorbar(cmap, ax=axs_arr[idx_r], orientation='vertical', label='Some Units')
 
         plt.tight_layout()
         plt.show()
+
