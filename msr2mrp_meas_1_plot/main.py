31ed89388d1d8fd0418ef72c0ca5359ab4e0109a
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-01-06 09:35:12 +0100

    split bar plot

--- msr2mrp_meas_1_plot/main.py
+++ msr2mrp_meas_1_plot/main.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--parameters', dest='params', nargs='+', help='Parameter space')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Debug mode')
     parser.add_argument('-pl', '--plot', dest='plot', choices=['network', 'bar','hist'], help='Plot type')
-    parser.add_argument('-pd', '--plot-data', dest='plot_data', choices=['disjoint', 'pathnum'], default='disjoint', help='Plot data')
+    parser.add_argument('-pd', '--plot-data', dest='plot_data', choices=['disjoint', 'pathnum', 'border', 'sinkpath'], default='disjoint', help='Plot data')
     parser.add_argument('-n', '--network', dest='network', type=str, nargs='*', help='Network plot files')
     parser.add_argument('-o', '--outlier', dest='outlier', type=float,  help='Outlier threshold', default=0.0)
     args = parser.parse_args()
@@ -68,48 +68,63 @@ if __name__ == '__main__':
         filename = args.filename.replace('$1', param)
         logger.debug('Base filename: ' + str(filename))
 
-        dmp = pd.read_pickle(filename.replace('$2', 'dmp'))
-        rm = pd.read_pickle(filename.replace('$2', 'rm'))
+        dmp  = pd.read_pickle(filename.replace('$2', 'dmp'))
+        rm   = pd.read_pickle(filename.replace('$2', 'rm'))
         sink = pd.read_pickle(filename.replace('$2', 'sink'))
         iso  = pd.read_pickle(filename.replace('$2', 'sink'))
-        record.append({'filename': filename, 'dmp':dmp, 'rm':rm, 'sink':sink, 'iso':iso})
+        role = pd.read_pickle(filename.replace('$2', 'role'))
+        record.append({'filename': filename, 'dmp':dmp, 'rm':rm, 'sink':sink, 'iso':iso, 'role':role})
 
     match args.plot:
         case 'bar':
-            fig, axs = plt.subplots(nrows=4, ncols=1)
-            sink_num = [ np.average(r['sink'].mean(axis=1)) for r in record ]
-            sink_err = [ np.average(r['sink'].std(axis=1)) for r in record ]
-            axs[0].set_title('Sink')
-            bars = axs[0].bar(args.params, sink_num)
-            axs[0].bar_label(bars, label_type='edge')
-            axs[0].errorbar(args.params, sink_num, sink_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4, capsize=2)
+            match args.plot_data:
+                case 'sinkpath':
+                    fig, axs = plt.subplots(nrows=2, ncols=1)
+                    sink_num = [np.average(r['sink'].mean(axis=1)) for r in record]
+                    sink_err = [np.average(r['sink'].std(axis=1)) for r in record]
+                    axs[0].set_title('Sink')
+                    bars = axs[0].bar(args.params, sink_num)
+                    axs[0].bar_label(bars, label_type='edge')
+                    axs[0].errorbar(args.params, sink_num, sink_err, fmt='.', color='Black', elinewidth=2, capthick=10,
+                                    errorevery=1, alpha=0.5, ms=4, capsize=2)
 
-            path_num = [ np.average(r['rm'].mean(axis=1)) for r in record ]
-            path_err = [ np.average(r['rm'].std(axis=1)) for r in record ]
-            axs[1].set_title('Path')
-            bars = axs[1].bar(args.params, path_num)
-            axs[1].bar_label(bars, label_type='edge')
-            axs[1].errorbar(args.params, path_num, path_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4, capsize=2)
+                    path_num = [np.average(r['rm'].mean(axis=1)) for r in record]
+                    path_err = [np.average(r['rm'].std(axis=1)) for r in record]
+                    axs[1].set_title('Path')
+                    bars = axs[1].bar(args.params, path_num)
+                    axs[1].bar_label(bars, label_type='edge')
+                    axs[1].errorbar(args.params, path_num, path_err, fmt='.', color='Black', elinewidth=2, capthick=10,
+                                    errorevery=1, alpha=0.5, ms=4, capsize=2)
+                    plt.tight_layout()
+                    plt.show()
+                case 'disjoint':
+                    fig, axs = plt.subplots(nrows=2, ncols=1)
 
-            dmp_num = [np.average(r['dmp'].mean(axis=1)) for r in record]
-            dmp_err = [np.average(r['dmp'].std(axis=1)) for r in record]
+                    dmp_num = [np.average(r['dmp'].mean(axis=1)) for r in record]
+                    dmp_err = [np.average(r['dmp'].std(axis=1)) for r in record]
 
-            axs[2].set_title('Disjointness')
-            bars = axs[2].bar(args.params, dmp_num)
-            axs[2].bar_label(bars, label_type='edge')
-            axs[2].errorbar(args.params, dmp_num, dmp_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4,
-                         capsize=2)
+                    axs[0].set_title('Disjointness')
+                    bars = axs[0].bar(args.params, dmp_num)
+                    axs[0].bar_label(bars, label_type='edge')
+                    axs[0].errorbar(args.params, dmp_num, dmp_err, fmt='.', color='Black', elinewidth=2, capthick=10,
+                                    errorevery=1, alpha=0.5, ms=4,
+                                    capsize=2)
 
-            two_disj_num = [ (r['dmp'] ==0).sum(axis=1).div(r['dmp'].notna().sum(axis=1)).mean() for r in record ]
-            two_disj_err =[ (r['dmp'] ==0).sum(axis=1).div(r['dmp'].notna().sum(axis=1)).std() for r in record ]
+                    two_disj_num = [(r['dmp'] == 0).sum(axis=1).div(r['dmp'].notna().sum(axis=1)).mean() for r in record]
+                    two_disj_err = [(r['dmp'] == 0).sum(axis=1).div(r['dmp'].notna().sum(axis=1)).std() for r in record]
 
-            axs[3].set_title('Two disj probability')
-            bars = axs[3].bar(args.params, two_disj_num)
-            axs[3].errorbar(args.params, two_disj_num, two_disj_err, fmt='.', color='Black', elinewidth=2, capthick=10, errorevery=1, alpha=0.5, ms=4,
-                         capsize=2)
-            axs[3].bar_label(bars, label_type='edge')
-            plt.tight_layout()
-            plt.show()
+                    axs[1].set_title('Two disj probability')
+                    bars = axs[1].bar(args.params, two_disj_num)
+                    axs[1].errorbar(args.params, two_disj_num, two_disj_err, fmt='.', color='Black', elinewidth=2, capthick=10,
+                                    errorevery=1, alpha=0.5, ms=4,
+                                    capsize=2)
+                    axs[1].bar_label(bars, label_type='edge')
+                    plt.tight_layout()
+                    plt.show()
+                case _:
+                    logger.error('Unknown plot type for bar: ' + args.plot_data)
+                    exit(1)
+
         case 'network':
             fig, axs = plt.subplots(nrows=int(np.ceil(len(record[1:]) / 2)), ncols=2)
             axs_arr = axs.ravel()
@@ -118,14 +133,23 @@ if __name__ == '__main__':
                 stream = open(args.network[idx_r+1], 'r')
                 loader = yaml.safe_load(stream)
                 nw = construct_graph(loader)
-                if args.plot_data == 'disjoint':
-                    color = [r['dmp'].mean(axis=0)[i] if i in r['dmp'].mean(axis=0).index and r['dmp'][i].notna().sum()/len(r['dmp'][i]) > args.outlier else 0 for i in nw.nodes()]
-                else:
-                    color = [r['rm'].mean(axis=0)[i] if i in r['rm'].mean(axis=0).index and r['dmp'][i].notna().sum()/len(r['dmp'][i]) > args.outlier else 0 for i in nw.nodes()]
-
+                alpha=1
+                match args.plot_data:
+                    case 'disjoint':
+                        color = [r['dmp'].mean(axis=0)[i] if i in r['dmp'].mean(axis=0).index and r['dmp'][i].notna().sum()/len(r['dmp'][i]) > args.outlier else 0 for i in nw.nodes()]
+                    case 'pathnum':
+                        color = [r['rm'].mean(axis=0)[i] if i in r['rm'].mean(axis=0).index and
+                                                    r['dmp'][i].notna().sum() / len(r['dmp'][i]) > args.outlier else 0 for i in nw.nodes()]
+                    case 'border':
+                        color = [r['role'][i].value_counts()['border']/r['role'][i].value_counts().sum() if 'border' in r['role'][i].value_counts() else 0 for i in nw.nodes()]
+                        alpha = [ 1 if i > 0 else 0 for i in color]
+                        edgecolors = [  '#1f78b4ff' for i in color]
+                    case _:
+                        'none'
+                edgecolors = ['#1f78b4ff' for i in color]
                 pos = nx.get_node_attributes(nw, 'pos')
-                edgecolors = []
-                nx.draw_networkx_nodes(nw, ax=axs_arr[idx_r], pos=pos, node_color=color, cmap='viridis', node_size=200)
+
+                nx.draw_networkx_nodes(nw, ax=axs_arr[idx_r], pos=pos, node_color=color, cmap='viridis', node_size=200, alpha=alpha, edgecolors=edgecolors)
                 nx.draw_networkx_edges(nw, ax=axs_arr[idx_r], pos=pos)
 
                 cmap = cm.ScalarMappable(cmap='viridis')
