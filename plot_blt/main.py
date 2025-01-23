e1e393fd43912c384cf5786a980f1f4e6afe94fa
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-01-23 09:44:53 +0100

    plot blt from pickle - debug log

--- plot_blt/main.py
+++ plot_blt/main.py
@@ -58,6 +58,7 @@ if __name__ == '__main__':
     for idx, data in enumerate(lts[args.source].items()):
         counts, bins = np.histogram(data[1],bins=20, range=(min_bin, max_bin))
         logger.debug('bins: '+str(bins))
+        logger.debug('counts: ' + str(counts))
         axs_arr[idx].hist(bins[:-1], weights=counts)
         axs_arr[idx].set_title(data[0])
         min_count.append(min(counts))
