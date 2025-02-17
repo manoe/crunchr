e95e9be7274a002f9cb06b781ff6fd2159612cd6
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-02-17 12:00:30 +0100

    new script

--- meas_4_plot_com/main.py
+++ meas_4_plot_com/main.py
@@ -30,6 +30,14 @@ def get_state_list(pkt_list):
     return get_attribute_list(pkt_list, 'state')
 
 
+def gen_diff(table):
+    res = pd.DataFrame(index=table.index, columns=table.columns[1:])
+
+    for i in table.index:
+        row = list(table.loc[i])
+        res.loc[i] = [b-a for a,b in zip(row[:-1], row[1:])]
+    return res
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='meas_4_plot_com', description='Plot protocol ', epilog=':-(')
     parser.add_argument('-f','--file', dest='filename', help='Filename')
@@ -47,7 +55,12 @@ if __name__ == '__main__':
 
     recv_pkt_list = get_recv_pkt_list(loader)
 
+    diff_recv_list = gen_diff(recv_pkt_list)
+
+    image = diff_recv_list.map(lambda x: 1 if x > 0 else 0, na_action='ignore')
+
+    fig, axs = plt.subplots(nrows=1, ncols=1, layout='compressed')
 
 
-#    fig, axs = plt.subplots(nrows=len(args.proto2), ncols=1, layout='compressed', figsize=(4, 12))
-
+    axs.imshow(image)
+    plt.show()
