2de5352ad90aa3197320106433c39fba25a7aa90
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-02-18 11:56:43 +0100

    animation works

--- meas_4_plot_ff/main.py
+++ meas_4_plot_ff/main.py
@@ -6,30 +6,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import logging
+from enum import Enum
 logger = logging.getLogger(__name__)
 import sys
 
 from matplotlib import rcParams
+
 rcParams['font.family'] = ['serif']
 rcParams['font.serif'] = ['Times New Roman']
 
+#enum CellState {
+#    NO_FUEL,
+#    NOT_IGNITED,
+#    BURNING,
+#    BURNED_DOWN
+#};
+
+class CellState(Enum):
+    NO_FUEL     = 0
+    NOT_IGNITED = 1
+    BURNING     = 2
+    BURNED_DOWN = 3
+
 def map_value(value):
     if value == '1':
-        return 0
+        return 0.0
     else:
-        return 1
+        return 1.0
+
 
 def gen_frame(plane):
-    logger.debug('Array\'s shape (x,y): ' + str((len(plane), len(str(plane[0])))))
-    frame = np.ndarray(shape=(len(plane), len(str(plane[0]))))
-    for idx,i in enumerate(plane):
-        frame[:,idx]=[map_value(j) for j in str(i['y'])]
+    logger.debug('Array\'s shape (x,y): ' + str((len(plane), len(str(plane[0]['y'])))))
+    frame = np.ndarray(shape=(len(plane), len(str(plane[0]['y']))))
+    for idx, i in enumerate(plane):
+        logger.debug('Array\'s idx: ' + str(idx))
+        frame[:, idx] = [map_value(j) for j in str(i['y'])]
     logger.debug(frame)
     return frame
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='meas_4_plot_ff', description='Plot ', epilog=':-(')
-    parser.add_argument('-f','--file', dest='filename', help='Filename')
+    parser.add_argument('-f', '--file', dest='filename', help='Filename')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Debug mode')
     args = parser.parse_args()
 
@@ -41,15 +59,9 @@ if __name__ == '__main__':
     stream = open(args.filename, 'r')
     loader = yaml.safe_load(stream)
 
-    fig, ax = plt.subplots()
-    rng = np.random.default_rng(19680801)
-    data = np.array([20, 20, 20, 20])
-    x = np.array([1, 2, 3, 4])
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    frames = [gen_frame(i['plane']) for i in loader]
+    artists = [ [ax.imshow(frame, animated=True)] for frame in frames]
+    ani = anm.ArtistAnimation(fig=fig, artists=artists, interval=400, repeat=True, blit=True, repeat_delay = 1000)
 
-    artists = []
-    for i in loader:
-        container = ax.imshow(gen_frame(i['plane']))
-        artists.append(container)
-
-    ani = anm.ArtistAnimation(fig=fig, artists=artists, interval=400)
     plt.show()
