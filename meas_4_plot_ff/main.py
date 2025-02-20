582784798b766d5d0427af4c2d2eb88033a10229
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-02-20 10:10:04 +0100

    network over image

--- meas_4_plot_ff/main.py
+++ meas_4_plot_ff/main.py
@@ -3,9 +3,11 @@ import matplotlib.animation as anm
 import yaml
 import numpy as np
 import matplotlib.pyplot as plt
+import networkx as nx
 import argparse
 import logging
 from enum import Enum
+
 logger = logging.getLogger(__name__)
 import sys
 
@@ -13,6 +15,7 @@ from matplotlib import rcParams
 
 rcParams['font.family'] = ['serif']
 rcParams['font.serif'] = ['Times New Roman']
+
 
 #enum CellState {
 #    NO_FUEL,
@@ -24,9 +27,9 @@ rcParams['font.serif'] = ['Times New Roman']
 #Hint: /home/uveges/devCastalia/Castalia/Simulations/shmrp_ff_event
 
 class CellState(Enum):
-    NO_FUEL     = 0
+    NO_FUEL = 0
     NOT_IGNITED = 1
-    BURNING     = 2
+    BURNING = 2
     BURNED_DOWN = 3
 
 
@@ -47,20 +50,66 @@ def map_value(value):
 
 
 def gen_frame(plane):
-    logger.debug('Array\'s shape (x,y): ' + str((len(plane), len(str(plane[0]['y'])))))
-    frame = np.ndarray(shape=(len(plane), len(str(plane[0]['y'])),4), dtype=np.uint8)
+    logger.debug('Array\'s shape (y,x): ' + str((len(plane), len(str(plane[0]['y'])))))
+    frame = np.ndarray(shape=(len(plane), len(str(plane[0]['y'])), 4), dtype=np.uint8)
     for idx, i in enumerate(plane):
         logger.debug('Array\'s idx: ' + str(idx))
-        frame[:, idx,:] = [map_value(j) for j in str(i['y'])]
+        frame[idx, :, :] = [map_value(j) for j in str(i['y'])]
     logger.debug(frame)
-    return np.flipud(frame)
+    return frame
 
+def construct_graph(run):
+    nw = nx.MultiDiGraph()
+    for node in run:
+        nw.add_node(node['node'], master=node['master'], pos=[node['x'], node['y']])
+        if 'engines' in node:
+            roles = []
+            for engine in node['engines']:
+                if 'routing_table' in engine:
+                    for re in engine['routing_table']:
+                        pathid = [pe['pathid'] for pe in re['pathid']]
+                        if engine['role'] == 'external':
+                            secl = False
+                            for k in re['pathid']:
+                                if k['secl']:
+                                    secl = True
+                            nw.add_edge(node['node'], re['node'], pathid=pathid, secl=secl, engine=engine['engine'])
+                        elif engine['role'] == 'border':
+                            if re['node'] in pathid:
+                                nw.add_edge(node['node'], re['node'], pathid=pathid, engine=engine['engine'])
+                            else:
+                                # ulgy hack
+                                nw.add_edge(node['node'], re['node'], pathid=[0], secl=re['secl'],
+                                            engine=engine['engine'])
+                        else:
+                            nw.add_edge(node['node'], re['node'], pathid=[], secl=re['secl'], engine=engine['engine'])
+                roles.append((engine['engine'], engine['role']))
+            if len(roles) == 0:
+                roles = ['none']
+            nx.set_node_attributes(nw, {node['node']: roles}, 'roles')
+    return nw
+
+
+def create_nw_color_list(nw):
+    return ['tab:blue' if 'external' in i[0][1] else 'tab:brown' if 'border' in i[0][1] else 'tab:pink' if 'central' in i[0][1] else 'tab:cyan'
+            for i in nx.get_node_attributes(nw, 'roles').values()]
+
+
+def nw_axes(nw, ax):
+    pos = nx.get_node_attributes(nw, 'pos')
+    n_alpha=1
+    n_axes = nx.draw_networkx_nodes(nw, ax=ax, pos=pos, alpha=n_alpha, node_color=create_nw_color_list(nw))
+    l_axes = nx.draw_networkx_labels(nw, ax=ax, pos=pos, font_size=9)
+    e_alpha=1
+    e_axes = nx.draw_networkx_edges(nw, ax=ax, pos=pos, alpha=e_alpha, connectionstyle="arc3,rad=0.2")
+    return [n_axes] +list(l_axes.values()) + e_axes
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='meas_4_plot_ff', description='Plot ', epilog=':-(')
     parser.add_argument('-f', '--file', dest='filename', help='Filename')
-    parser.add_argument('-v','--video', dest='video', action='store_true', help='Write video')
+    parser.add_argument('-v', '--video', dest='video', action='store_true', help='Write video')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Debug mode')
+    parser.add_argument('-n', '--network', dest='network', action='store_true', default=False, help='Plot also network')
     args = parser.parse_args()
 
     if args.debug:
@@ -73,9 +122,20 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots(nrows=1, ncols=1, layout='compressed')
 
-    frames = [gen_frame(i['plane']) for i in loader]
-    artists = [ [ax.imshow(frame, animated=True)] for frame in frames]
-    ani = anm.ArtistAnimation(fig=fig, artists=artists, interval=400, repeat=True, blit=True, repeat_delay = 1000)
+    frames = [gen_frame(i['plane']['plane']) for i in loader['nrg_list']]
+
+    #x = np.arange(0, 100)
+    #im, = ax.plot(x, x) # what does the , do?
+
+    artists = [ax.imshow(frame, animated=True, origin='lower', zorder=0) for frame in frames]
+
+    if args.network:
+        nw_frames = [construct_graph(i['routing']) for i in loader['nrg_list']]
+        nw_artists = [ nw_axes(nw,ax) for nw in nw_frames ]
+        artists = [ nw_artist+[artist] for nw_artist, artist in zip(nw_artists, artists) ]
+
+    ani = anm.ArtistAnimation(fig=fig, artists=artists, interval=400, repeat=True, blit=True, repeat_delay=1000)
+
 
     if args.video:
         ani.save("out.gif", dpi=300, writer=anm.PillowWriter(fps=2))
