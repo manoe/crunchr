e34fb541975f6b031bc65e7a09a4181925da8b22
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-02-24 09:21:48 +0100

    Speed up fps

--- meas_4_plot_ff/main.py
+++ meas_4_plot_ff/main.py
@@ -181,6 +181,6 @@ if __name__ == '__main__':
     ani = anm.ArtistAnimation(fig=fig, artists=artists, interval=400, repeat=True, blit=True, repeat_delay=1000)
 
     if args.video:
-        ani.save("out.gif", dpi=args.resolution, writer=anm.PillowWriter(fps=2))
+        ani.save("out.gif", dpi=args.resolution, writer=anm.PillowWriter(fps=4))
     else:
         plt.show()
