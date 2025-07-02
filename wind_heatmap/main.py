9129c56f9f9da83d87b888713113cebc4fc46e5d
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-07-02 14:24:45 +0200

    wind "heat" map

---
+++ wind_heatmap/main.py
@@ -0,0 +1,49 @@
+#!/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+logger = logging.getLogger(__name__)
+import sys
+
+from matplotlib import rcParams
+rcParams['font.family'] = ['serif']
+rcParams['font.serif'] = ['Times New Roman']
+rcParams['font.size'] = 12
+
+def f_w_a(angle, c_w_a=0.3):
+    if not c_w_a < 1.0:
+        raise Exception('c_w_a must be > 1.0')
+
+    return c_w_a * np.sign(np.cos(angle)) * np.pow(np.cos(angle),2)
+
+def f_w_s(speed, c_w_s=1.0, c_w_l=0.0, c_w_m=1.0):
+    return c_w_s * sigmoid((12.0 + c_w_l)/c_w_m * speed - 6 - c_w_l)
+
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+if __name__ == '__main__':
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+    logger.info('Loading data')
+    angle_arr = np.linspace(0, 2 * np.pi, 100)
+    speed_arr = np.linspace(0, 1, 100)
+
+    angle_tick_arr = np.linspace(0, 2 * np.pi, 10)
+    speed_tick_arr = np.linspace(0, 1, 10)
+
+    value_map = np.ndarray(shape=(len(angle_arr), len(speed_arr)))
+    for idx_angle, angle in enumerate(angle_arr):
+        for idx_speed, speed in enumerate(speed_arr):
+            value_map[idx_angle, idx_speed] = 1/(1 - f_w_a(angle) * f_w_s(speed))
+    fig = plt.figure(figsize=(9, 8) )
+    ax = plt.subplot(1,1,1)
+    im = ax.imshow(value_map, origin='lower')
+    cax = cbar_ax = fig.add_axes([0.88, 0.15, 0.04, 0.7])
+    fig.colorbar(im,cax=cax)
+    ax.set_yticks(np.arange(0, len(angle_arr), len(angle_arr)/10))
+    ax.set_xticks(np.arange(0, len(speed_arr), len(angle_arr)/10))
+    ax.set_yticklabels([ '{:,.2f}'.format(i) for i in np.linspace(0, 2 * np.pi, 1+len(ax.get_yticks()))][:-1])
+    ax.set_xticklabels([ '{:,.2f}'.format(i) for i in np.linspace(0, 1, 1+len(ax.get_xticks()))][:-1])
+
+    plt.show()
