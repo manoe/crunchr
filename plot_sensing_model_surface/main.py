629fce26001ba6b0c5763a32c6e5df5e1e21b8ee
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-08-27 15:01:46 +0200

    plot sensing models

--- plot_sensing_model_surface/main.py
+++ plot_sensing_model_surface/main.py
@@ -10,7 +10,7 @@ from matplotlib.ticker import FuncFormatter
 logger = logging.getLogger(__name__)
 
 from matplotlib import rcParams
-
+from matplotlib import cm
 rcParams['font.family'] = ['serif']
 rcParams['font.serif'] = ['Times New Roman']
 rcParams['font.size'] = 12
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--file', dest='filename',
                         help='Filename, use $0, $1 , $2 for param 1, param 2, and $3, for seed, respectively.')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Log level')
-    parser.add_argument('-p', '--smod', dest='smod', type=str, help='Sensing model, $0', nargs='+')
+    #parser.add_argument('-p', '--smod', dest='smod', type=str, help='Sensing model, $0', nargs='+')
     parser.add_argument('-p1', '--param-1', dest='param1', type=str, help='Parameter 1, $1', nargs='+')
     parser.add_argument('-l', '--labels', dest='labels', type=str, help='Labels for param 1', nargs='*')
-    parser.add_argument('-p2', '--param-2', dest='param2', type=str, help='Parameter 2, $2', nargs='+')
+#    parser.add_argument('-p2', '--param-2', dest='param2', type=str, help='Parameter 2, $2', nargs='+')
     parser.add_argument('-s', '--seeds', dest='seeds', type=str, help='Seeds, $3', nargs='+')
     parser.add_argument('-o', '--out', dest='out_file', type=str, help='Out file', default='out')
     args = parser.parse_args()
@@ -32,54 +32,55 @@ if __name__ == '__main__':
     if args.debug:
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-    res = {p + '_' + p1 + '_' + p2: {'r': [], 'l': [], 'd': []} for p in args.smod for p1 in args.param1 for p2 in
-           args.param2}
+    res = {p1: {'r': [], 'l': [], 'd': [], 'm': []} for p1 in args.param1 }
 
-    for p in args.smod:
-        for p1 in args.param1:
-            for p2 in args.param2:
-                filename = args.filename.replace('$0', p).replace('$1', p1).replace('$2', p2)
-                logger.debug('Init filename: ' + str(filename))
-                tables = {i: pd.DataFrame() for i in 'rld'}
-                for s in args.seeds:
-                    data = pd.read_pickle(filename.replace('$3', s))
-                    logger.debug('seed: ' + str(s))
-                    for d in data.columns:
-                        tables[d][s] = data[d].values
-                        timestamps = data[d].index.values
-                for i in 'rld':
-                    #res[p + '_' + p1 + '_' + p2][i].append(tables[i].mean(axis=1))
-                    res[p + '_' + p1 + '_' + p2][i]=tables[i].mean(axis=1)
-    for p in args.smod:
-        del res[p+'_false'+'_true']
+    #for p in args.smod:
+    for p1 in args.param1:
+        #filename = args.filename.replace('$0', p).replace('$1', p1)
+        filename = args.filename.replace('$1', p1)
+        logger.debug('Init filename: ' + str(filename))
+        tables = {i: pd.DataFrame() for i in 'rldm'}
+        for s in args.seeds:
+            data = pd.read_pickle(filename.replace('$3', s))
+            logger.debug('seed: ' + str(s))
+            for d in data.columns:
+                tables[d][s] = data[d].values
+                timestamps = data[d].index.values
+        for i in 'rld':
+            #res[p + '_' + p1 + '_' + p2][i].append(tables[i].mean(axis=1))
+            res[p1][i]=tables[i].mean(axis=1)
 
     if args.labels:
         if len(args.labels) != len(res.keys()):
             logger.error('Number of labels does not match product number of parameters')
             exit(1)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, layout='compressed')
-    for k, color in zip(res.keys(), TABLEAU_COLORS):
-        ax.plot(timestamps, res[k]['r'], color=color)
-        #for d, l in zip(['r', 'l', 'd'], ['-', ':', '--']):
-        #    ax.plot(timestamps, res[k][d], color=color, ls=l)
-    for t, color in zip([args.smod[0]+'_true_true', args.smod[0]+'_false_false'], list(TABLEAU_COLORS.keys())[len(res.keys()):]):
-        for d,l in zip(['l','d'], [':','--']):
-            ax.plot(timestamps, res[t][d], color=color, ls=l)
-    node_state_labels = ['Living nodes-Limited mobility'] + ['Destroyed nodes -Limited mobility'] + ['Living nodes-No mobility'] + ['Destroyed nodes-No mobility']
-    if args.labels:
-        labels=args.labels+node_state_labels
-    else:
-        labels=list(res.keys())+node_state_labels
-    ax.legend(labels, loc='lower left', bbox_to_anchor=(0, 0.1))
-    ax.set_xlabel('Time (min)')
-    ax.set_ylabel('Node number')
-    #ax.set_xticklabels([i for i in ax.get_xticklabels()])
-    ax.set_xlim([0, max(timestamps)])
-    ax.xaxis.set_major_formatter(FuncFormatter(lambda x, p: '{0:.0f}'.format(x/60)))
-    ax.grid(True)
-    plt.savefig(args.out_file + '.pdf', dpi=300)
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
+    #fig, ax = plt.subplots(nrows=1, ncols=1, layout='compressed')
+
+    X = timestamps
+    Y = [int(idx) for idx, i in enumerate(list(args.param1))]
+
+    X, Y = np.meshgrid(X, Y)
+
+    Z = np.empty((len(X),len(Y[0])))
+
+    for idx, i in enumerate(args.param1):
+        Z[idx] = res[i]['r']
+
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    ax.set_yticks([int(idx) for idx, i in enumerate(list(args.param1))])
+    ticklabels = ax.get_yticklabels()
+    ticklabels[len(ticklabels) - len(list(args.param1)):] = list(args.param1)
+
+    ax.set_yticklabels(ticklabels)
+
+
+    plt.savefig(args.out_file + '.pdf', dpi=300)
+    plt.show()
 
 #import matplotlib.pyplot as plt
 #import numpy as np
