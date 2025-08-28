38d71cf9adc7627868af9ba6f2c7d4ff5c5bcde5
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-08-28 17:04:22 +0200

    typo

--- plot_sensing_model_surface/main.py
+++ plot_sensing_model_surface/main.py
@@ -8,6 +8,8 @@ import logging
 from matplotlib.colors import TABLEAU_COLORS
 from matplotlib.ticker import FuncFormatter
 logger = logging.getLogger(__name__)
+from scipy.interpolate import interp1d, pchip
+from scipy.signal import savgol_filter
 
 from matplotlib import rcParams
 from matplotlib import cm
@@ -27,6 +29,7 @@ if __name__ == '__main__':
 #    parser.add_argument('-p2', '--param-2', dest='param2', type=str, help='Parameter 2, $2', nargs='+')
     parser.add_argument('-s', '--seeds', dest='seeds', type=str, help='Seeds, $3', nargs='+')
     parser.add_argument('-o', '--out', dest='out_file', type=str, help='Out file', default='out')
+    parser.add_argument('-a', '--attribute', dest='attribute', type=str, help='Out file', default='r', choices=['r', 'l', 'b', 'm', 'e'])
     args = parser.parse_args()
 
     if args.debug:
@@ -39,14 +42,14 @@ if __name__ == '__main__':
         #filename = args.filename.replace('$0', p).replace('$1', p1)
         filename = args.filename.replace('$1', p1)
         logger.debug('Init filename: ' + str(filename))
-        tables = {i: pd.DataFrame() for i in 'rldm'}
+        tables = {i: pd.DataFrame() for i in 'rldme'}
         for s in args.seeds:
             data = pd.read_pickle(filename.replace('$3', s))
             logger.debug('seed: ' + str(s))
             for d in data.columns:
                 tables[d][s] = data[d].values
                 timestamps = data[d].index.values
-        for i in 'rld':
+        for i in 'rldme':
             #res[p + '_' + p1 + '_' + p2][i].append(tables[i].mean(axis=1))
             res[p1][i]=tables[i].mean(axis=1)
 
@@ -62,23 +65,29 @@ if __name__ == '__main__':
     X = timestamps
     Y = [int(idx) for idx, i in enumerate(list(args.param1))]
 
-    X, Y = np.meshgrid(X, Y)
-
-    Z = np.empty((len(X),len(Y[0])))
-
     for idx, i in enumerate(args.param1):
-        Z[idx] = res[i]['r']
+        yhat = savgol_filter(res[i][args.attribute], 2, 1)
+        ax.plot(xs=X, ys=yhat, zs=idx, zdir='y', alpha=0.8)
+        # ax.plot(xs=X, ys = res[i][args.attribute], zs=idx, zdir='y', alpha=0.8)
+#    X, Y = np.meshgrid(X, Y)
+#
+#    Z = np.empty((len(X),len(Y[0])))
+#
+#    for idx, i in enumerate(args.param1):
+#        Z[idx] = res[i][args.attribute]
+#
+#    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+#    fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+#    ax.set_yticks([int(idx) for idx, i in enumerate(list(args.param1))])
+#    ticklabels = ax.get_yticklabels()
+#    ticklabels[len(ticklabels) - len(list(args.param1)):] = list(args.param1)
+#
+#    ax.set_yticklabels(ticklabels)
 
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-
-    ax.set_yticks([int(idx) for idx, i in enumerate(list(args.param1))])
-    ticklabels = ax.get_yticklabels()
-    ticklabels[len(ticklabels) - len(list(args.param1)):] = list(args.param1)
-
-    ax.set_yticklabels(ticklabels)
-
-
+    ax.set_yticks(Y)
+    ax.set_yticklabels(args.param1)
+    ax.legend(args.param1)
     plt.savefig(args.out_file + '.pdf', dpi=300)
     plt.show()
 
