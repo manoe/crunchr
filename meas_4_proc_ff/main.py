33b27bd0ad6fe09a0820476a29443ccd9270745a
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2025-09-03 14:02:11 +0200

    typo

--- meas_4_proc_ff/main.py
+++ meas_4_proc_ff/main.py
@@ -10,12 +10,15 @@ logger = logging.getLogger(__name__)
 import sys
 
 
-def gen_diff(table):
+def gen_diff(table, to_float=False):
     res = pd.DataFrame(index=table.index, columns=table.columns[1:])
 
     for i in table.index:
         row = list(table.loc[i])
-        res.loc[i] = [b-a for a,b in zip(row[:-1], row[1:])]
+        if(to_float):
+            res.loc[i] = [float(b - a) for a, b in zip(row[:-1], row[1:])]
+        else:
+            res.loc[i] = [b-a for a,b in zip(row[:-1], row[1:])]
     return res
 
 
@@ -48,7 +51,13 @@ if __name__ == '__main__':
     pkts = pd.DataFrame(index=[i['node'] for i in loader['nodes'] if i['role'] != 'central'],
                              columns=[i for i in np.arange(-1, args.count)], data=0)
 
+    s_pkts = pd.DataFrame(index=[i['node'] for i in loader['nodes'] if i['role'] != 'central'],
+                             columns=[i for i in np.arange(-1, args.count)], data=0)
+
     e_pkts = pd.DataFrame(index=[i['node'] for i in loader['nodes'] if i['role'] != 'central'],
+                        columns=[i for i in np.arange(-1, args.count)], data=0)
+
+    s_e_pkts = pd.DataFrame(index=[i['node'] for i in loader['nodes'] if i['role'] != 'central'],
                         columns=[i for i in np.arange(-1, args.count)], data=0)
 
     living = pd.DataFrame(index=[i['node'] for i in loader['nodes'] if i['role'] != 'central'],
@@ -56,6 +65,8 @@ if __name__ == '__main__':
 
     mobility = pd.DataFrame(index=[i['node'] for i in loader['nodes']],
                                   columns=[i for i in np.arange(0, args.count)], data=False)
+
+
 
     timestamps = []
     for i in range(args.count):
@@ -68,24 +79,42 @@ if __name__ == '__main__':
         timestamps.append(timestamp)
 
         pkts[i] = get_attribute_list(loader['nodes'], 'report_recv')
+        s_pkts[i] = get_attribute_list(loader['nodes'], 'report_sent')
         e_pkts[i] = get_attribute_list(loader['nodes'], 'event_recv')
+        s_e_pkts[i] = get_attribute_list(loader['nodes'], 'event_sent')
         living[i] = get_attribute_list(loader['nodes'], 'state').map(lambda x: True if x == 'live' else False)
         mobility[i] = get_attribute_list(loader['nodes'], 'mobility')
+
     reachable = gen_diff(pkts).map(lambda x: True if x > 0 else False)
     e_reachable = gen_diff(e_pkts).map(lambda x: True if x > 0 else False)
 
     reachable_count = [0 if True not in reachable[i].value_counts() else reachable[i].value_counts()[True]  for i in reachable]
     e_reachable_count = [0 if True not in e_reachable[i].value_counts() else e_reachable[i].value_counts()[True] for i in e_reachable]
 
+    r_recv = gen_diff(pkts, to_float=True)
+    r_sent = gen_diff(s_pkts, to_float=True)
+    e_recv = gen_diff(e_pkts, to_float=True)
+    e_sent = gen_diff(s_e_pkts, to_float=True)
+
+    r_pdr = (r_recv/r_sent.replace(0.0, None)).replace(np.nan,0)
+    e_pdr = (e_recv/e_sent.replace(0.0, None)).replace(np.nan,0)
+
+    avg_r_pdr = r_pdr.mean(axis=0)
+    avg_e_pdr = e_pdr.mean(axis=0)
+
     living_count = [living[i].value_counts()[True] if True in living[i].value_counts() else 0 for i in living]
     dead_count = [living[i].value_counts()[False] if False in living[i].value_counts() else 0 for i in living]
     mobility_count  = [mobility[i].value_counts()[True] if True in mobility[i].value_counts() else 0 for i in mobility]
 
 
-    out=pd.DataFrame(index=timestamps)
+    out=pd.DataFrame(index=timestamps, dtype=float)
+
     out['r']=reachable_count
     out['e']=e_reachable_count
     out['l']=living_count
     out['d']=dead_count
     out['m']=list(it.accumulate(mobility_count))
+    out['pr']=avg_r_pdr.values
+    out['pe']=avg_e_pdr.values
+
     out.to_pickle(args.out_file+'.pickle')
---
+++ plot_sensing_model_data/main.py
@@ -0,0 +1,155 @@
+#!/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import argparse
+import logging
+from matplotlib.colors import TABLEAU_COLORS
+from matplotlib.ticker import FuncFormatter
+logger = logging.getLogger(__name__)
+from scipy.interpolate import interp1d, pchip
+from scipy.signal import savgol_filter
+
+from matplotlib import rcParams
+from matplotlib import cm
+rcParams['font.family'] = ['serif']
+rcParams['font.serif'] = ['Times New Roman']
+rcParams['font.size'] = 12
+import sys
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='plot_sensing_model_surface', description='Process ff related data ', epilog=':-(')
+    parser.add_argument('-f', '--file', dest='filename',
+                        help='Filename, use $0, $1 , $2 for param 1, param 2, and $3, for seed, respectively.')
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False, help='Log level')
+    #parser.add_argument('-p', '--smod', dest='smod', type=str, help='Sensing model, $0', nargs='+')
+    parser.add_argument('-p1', '--param-1', dest='param1', type=str, help='Parameter 1, $1', nargs='+')
+    parser.add_argument('-l', '--labels', dest='labels', type=str, help='Labels for param 1', nargs='*')
+    #    parser.add_argument('-p2', '--param-2', dest='param2', type=str, help='Parameter 2, $2', nargs='+')
+    parser.add_argument('-s', '--seeds', dest='seeds', type=str, help='Seeds, $3', nargs='+')
+    parser.add_argument('-o', '--out', dest='out_file', type=str, help='Out file', default='out')
+    parser.add_argument('-a', '--attribute', dest='attribute', type=str, help='Plot attribute', default='r', choices=['r', 'l', 'b', 'm', 'e'])
+    parser.add_argument('-m', '--limit', dest='limit', type=int, help='Limit frame processing to nth frame')
+    parser.add_argument('-e', '--extend', dest='extend', action='store_true', help='Extend series to match the longest series')
+    parser.add_argument('-w', '--window', dest='window', action='store_true', help='Plot multiple windows (not implemented)')
+    parser.add_argument('-std', '--standard-deviation', dest='std', action='store_true',
+                        help='Calculate STD instead of mean')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    res = {p1: {'r': [], 'l': [], 'd': [], 'm': []} for p1 in args.param1 }
+    res_std = {p1: {'r': [], 'l': [], 'd': [], 'm': []} for p1 in args.param1 }
+    #for p in args.smod:
+    for p1 in args.param1:
+        #filename = args.filename.replace('$0', p).replace('$1', p1)
+        filename = args.filename.replace('$1', p1)
+        logger.debug('Init filename: ' + str(filename))
+        tables = {i: pd.DataFrame() for i in 'rldme'}
+        max_len = 0
+        for s in args.seeds:
+            data = pd.read_pickle(filename.replace('$3', s))
+            logger.debug('seed: ' + str(s))
+            for d in data.columns:
+                if args.limit:
+                    tables[d][s] = data[d].iloc[:args.limit].values
+                    timestamps = data[d].iloc[:args.limit].index.values
+                else:
+                    #tables[d][s] = data[d].values
+                    tables[d] = pd.concat([tables[d], pd.Series(data[d].values) ], axis=1)
+                    if 'timestamps' not in locals() or len(timestamps) < len(data[d].index.values):
+                        timestamps = data[d].index.values
+                    if len(data[d].values) > max_len:
+                        max_len = len(data[d].values)
+
+        for i in 'rldme':
+            #res[p + '_' + p1 + '_' + p2][i].append(tables[i].mean(axis=1))
+            res_std[p1][i] = tables[i].std(axis=1)
+            res[p1][i] = tables[i].mean(axis=1)
+
+    for p1 in args.param1:
+        for i in 'rldme':
+            res[p1][i] = pd.concat([ res[p1][i], pd.Series([res[p1][i].iloc[-1] for j in range(len(res[p1][i]), max_len)])], ignore_index=True)
+
+    logger.debug('Max len: ' + str(max_len))
+
+    if args.labels:
+        if len(args.labels) != len(res.keys()):
+            logger.error('Number of labels does not match product number of parameters')
+            exit(1)
+
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+
+    #fig, ax = plt.subplots(nrows=1, ncols=1, layout='compressed')
+
+    X = timestamps
+    #    Y = [int(idx) for idx, i in enumerate(list(args.param1))]
+
+    handle_list = []
+    for idx, i in enumerate(args.param1):
+        handle, = ax.plot(X, res[i][args.attribute], alpha=0.8)
+        handle_list.append(handle)
+        if args.std:
+            ax.fill_between(X, res[i][args.attribute] - res_std[i][args.attribute],
+                            res[i][args.attribute] + res_std[i][args.attribute], color='#888888', alpha=0.2)
+
+
+            print('Std of '+str(i)+' std: '+str(np.std(res_std[i][args.attribute])))
+
+    ax.legend(handle_list, args.param1)
+    plt.savefig(args.out_file + '.pdf', dpi=300)
+    plt.show()
+        # ax.plot(xs=X, ys = res[i][args.attribute], zs=idx, zdir='y', alpha=0.8)
+#    X, Y = np.meshgrid(X, Y)
+#
+#    Z = np.empty((len(X),len(Y[0])))
+#
+#    for idx, i in enumerate(args.param1):
+#        Z[idx] = res[i][args.attribute]
+#
+#    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+#    fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+#    ax.set_yticks([int(idx) for idx, i in enumerate(list(args.param1))])
+#    ticklabels = ax.get_yticklabels()
+#    ticklabels[len(ticklabels) - len(list(args.param1)):] = list(args.param1)
+#
+#    ax.set_yticklabels(ticklabels)
+
+ #   ax.set_yticks(Y)
+ #   ax.set_yticklabels(args.param1)
+
+#import matplotlib.pyplot as plt
+#import numpy as np
+#
+#from matplotlib import cm
+#from matplotlib.ticker import LinearLocator
+#
+#
+#if __name__ == '__main__':
+#    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+#
+#    # Make data.
+#    X = np.arange(-5, 5, 0.25)
+#    Y = np.arange(-5, 5, 0.25)
+#    X, Y = np.meshgrid(X, Y)
+#    R = np.sqrt(X**2 + Y**2)
+#    Z = np.arctan(R)
+#
+#    # Plot the surface.
+#    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+#                           linewidth=0, antialiased=False)
+#
+#    # Customize the z axis.
+#    ax.set_zlim(-1.01, 1.01)
+#    ax.zaxis.set_major_locator(LinearLocator(10))
+#    # A StrMethodFormatter is used automatically
+#    ax.zaxis.set_major_formatter('{x:.02f}')
+#
+#    # Add a color bar which maps values to colors.
+#    fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+#    plt.show()
