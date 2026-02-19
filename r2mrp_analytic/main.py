528db90dbdfaf7186c94ef916a6abeb60413d46d
Balint Aron Uveges <uveges.balint.aron@itk.ppke.hu>
2026-02-19 23:25:51 +0100

    update

--- r2mrp_analytic/main.py
+++ r2mrp_analytic/main.py
@@ -12,11 +12,7 @@ import sys
 
 def calc_pd(dist):
     logger.info('dist={}'.format(dist))
-    lres = math.log(args.rd/dist, 10)
-    if lres < -1:
-        res = 0
-    else:
-        res = 1 + lres
+    res = 1.0 / (1.0 + math.exp(alpha*(dist-args.d0)))
     logger.info('res={}'.format(res))
     return res
 
@@ -39,7 +35,7 @@ def calc_e_p(nodes):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='r2mrp_analytic', description='Calculate R2MRP-specific data analytically', epilog=':-(')
-    parser.add_argument('-d', '--data', dest='data', choices=['e_p'], help='Data that should be calculated', default='e_p')
+    parser.add_argument('-d', '--data', dest='data', choices=['e_p', 'alpha'], help='Data that should be calculated', default='e_p')
     parser.add_argument('-n', '--node-num', dest='node', type=int, help='Number of nodes', default=100)
     parser.add_argument('-x', '--x-num', dest='x', type=int, help='Number of grid points along axis X', default=10)
     parser.add_argument('-y', '--y-num', dest='y', type=int, help='Number of grid points along axis Y', default=10)
@@ -47,9 +43,14 @@ if __name__ == '__main__':
     parser.add_argument('-gd','--grid-distance', dest='grid_distance', type=float, help='Grid cell distance in meters', default=20)
     parser.add_argument('-q', '--qos', dest='qos', type=float, help='QoS filter value', default=0.8)
     parser.add_argument('-nt', '--num-test', dest='nt', type=int, help='Number of link test messages', default=10)
-    parser.add_argument('-rd', '--ref_dist', dest='rd', type=float, help='Reference distance', default=10)
+    parser.add_argument('-rd', '--ref_dist', dest='d0', type=float, help='Reference distance', default=1)
+    parser.add_argument('-a', '--alpha', dest='alpha', type=float, help='Attenuation coefficient', default=2.5)
+    parser.add_argument('-i', '--iter', dest='iter', type=int, help='Number of iterations, if alpha is calculated', default=100)
 
     args = parser.parse_args()
+
+    alpha = 2.5
+
 
     match args.log_level:
         case 'debug':
@@ -60,17 +61,26 @@ if __name__ == '__main__':
     if args.x * args.y < args.node:
         logging.error('Number of grid points lower than number of nodes')
 
+    logger.info('Constructing node array')
+    nodes = []
+    for i in range(args.x):
+        for j in range(args.y):
+            if i * args.x + j < args.node:
+                nodes.append({'num': i * args.x + j, 'x': i * args.grid_distance, 'y': j * args.grid_distance, })
+
     match args.data:
         case 'e_p':
             print('Calculating E[P], expected number of paths:')
-            logger.info('Constructing node array')
-            nodes = []
-            for i in range(args.x):
-                for j in range(args.y):
-                    if i*args.x+j < args.node:
-                        nodes.append( {'num': i*args.x+j, 'x': i*args.grid_distance, 'y': j*args.grid_distance} )
-
             logger.info('Node array done, size:{}'.format(len(nodes))+' on a field of {}'.format(args.x)+'x{}'.format(args.y))
             print('E[P]={}'.format(calc_e_p(nodes)))
+        case 'alpha':
+            print('Calculating alpha values:')
+            print('Setting initial alpha values for each node')
+            for i,n in enumerate(nodes[1:]):
+                nodes[i]['alpha'] = { j: 0.0 for j in range(1,args.node) }
+                nodes[i]['alpha'][str(n['num'])]=calc_q_us(calc_dist(n))
 
+            print('Starting iteration')
+            for it in range(args.iter):
+                for i,n in enumerate(nodes[1:]):
 
